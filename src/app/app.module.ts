d2075cb45853838a86d8251464b978b511d07cc4
Yuriy-27 <yuriyshchebetun@gmail.com>
2019-08-15 22:01:13 +0300

    add some components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { AsideComponent } from './aside/aside.component';
+import { ContentComponent } from './content/content.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent,
+    AsideComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule,
