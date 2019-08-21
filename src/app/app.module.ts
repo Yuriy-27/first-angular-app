199fb25cbe79d3e32ce44a42eee37611d7b22772
Yuriy-27 <yuriyshchebetun@gmail.com>
2019-08-21 21:55:45 +0300

    add cources-details component and add routing path with :id

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
-import { AsideComponent } from './aside/aside.component';
-import { MainLayoutComponent } from './main-layout/main-layout.component';
-import { CoursesComponent } from './courses/courses.component';
-import { MySkillsComponent } from './my-skills/my-skills.component';
 import { LoginComponent } from './login/login.component';
-import { ContactsComponent } from './contacts/contacts.component';
-import { ExamplesComponent } from './examples/examples.component';
-import { NotFoundComponent } from './not-found/not-found.component';
-import { HomeComponent } from './home/home.component';
 import { MainLayoutModule } from './main-layout/main-layout.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
--- src/app/main-layout/main-layout-routing.module.ts
+++ src/app/main-layout/main-layout-routing.module.ts
@@ -2,12 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { MainLayoutComponent } from './main-layout.component';
 import { HomeComponent } from '../home/home.component';
-import { MySkillsComponent } from '../my-skills/my-skills.component';
-import { CoursesComponent } from '../courses/courses.component';
-import { ExamplesComponent } from '../examples/examples.component';
-import { ContactsComponent } from '../contacts/contacts.component';
 import { NotFoundComponent } from '../not-found/not-found.component';
-
 
 const routes: Routes = [
   {
@@ -15,7 +10,7 @@ const routes: Routes = [
       {path: '', redirectTo: 'home', pathMatch: 'full'},
       {path: 'home', component: HomeComponent},
       {path: 'skills', loadChildren: 'src/app/my-skills/my-skills.module#MySkillsModule'},
-      {path: 'education', loadChildren: 'src/app/courses/courses.module#CoursesModule'},
+      {path: 'courses', loadChildren: 'src/app/courses/courses.module#CoursesModule'},
       {path: 'examples', loadChildren: 'src/app/examples/examples.module#ExamplesModule'},
       {path: 'contacts', loadChildren: 'src/app/contacts/contacts.module#ContactsModule'}
     ]
